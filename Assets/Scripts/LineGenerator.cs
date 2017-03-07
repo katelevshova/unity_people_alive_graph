d69470980d7b0eb1cdd336661c47744d03b4a214
Kate Levshova <11220246+katelevshova@users.noreply.github.com>
2017-03-06 19:53:44 -0600

    Changed size of the image to 500x500, renamed variables, added comments

--- Assets/Scripts/LineGenerator.cs
+++ Assets/Scripts/LineGenerator.cs
@@ -8,8 +8,8 @@ public class LineGenerator : MonoBehaviour
     {
         public int birthYear = 0;
         public int deathYear = 0;
-        public int livePeriod = 0;
-        public float liveLineHeight = 0f;
+        public int liveYears = 0;
+        public float liveLineHeight = 0f; // pixels which are corresponding to liveYears
 
         public Person()
         {
@@ -18,20 +18,20 @@ public class LineGenerator : MonoBehaviour
 
     public Color graphColor = Color.yellow;
     [SerializeField]
-    Texture2D _texture;
+    private Texture2D _texture;
 
     private const int MIN_SIZE = 100; // min size of the Image in Editor
-    private const int PERIOD_START = 1900;
-    private const int PERIOD_END = 2000;
+    private const int PERIOD_START_YEAR = 1900;
+    private const int PERIOD_END_YEAR = 2000;
 
     private int _peopleAmount = 100; // equals the width of LineGenerator image in Editor
-    private int _maxLineHeight = 0; // the max height of line visible on the graph
-    private int _maxLivePeriod = 0;
+    private int _maxLineHeight = 0; // the max height of line visible on the graph in pixels
+    private int _maxLiveYears = 0;  // the max live period in years
     private Queue<Person> _peopleQueue;
-    private Vector2 _bottomLeftPos = new Vector2(0f, 0f);
+    private Vector2 _bottomLeftPos = new Vector2(0f, 0f); // the bottom left position of Image
 
-    private int _maxYear = PERIOD_END;
-    private int _minYear = PERIOD_START;
+    private int _maxYear = PERIOD_END_YEAR;
+    private int _minYear = PERIOD_START_YEAR;
 
     // Use this for initialization
     void Start()
@@ -49,20 +49,20 @@ public class LineGenerator : MonoBehaviour
             throw new System.Exception("You need to specify Width and Height in Editor for the LineRenderer");
         }
 
-        if (PERIOD_END <= PERIOD_START)
+        if (PERIOD_END_YEAR <= PERIOD_START_YEAR)
         {
-            throw new System.Exception("PERIOD_END must be grater than PERIOD_START");
+            throw new System.Exception("PERIOD_END_YEAR must be grater than PERIOD_START_YEAR");
         }
 
         _bottomLeftPos = rect.offsetMin;
         _peopleAmount = (int)rect.rect.width;
-        _maxLineHeight = (int)rect.rect.height;     // Image's heightin px which corresponds to _maxLivePeriod in years
-        _maxLivePeriod = PERIOD_END - PERIOD_START;
+        _maxLineHeight = (int)rect.rect.height;     // Image's height in px which corresponds to _maxLiveYears in years
+        _maxLiveYears = PERIOD_END_YEAR - PERIOD_START_YEAR;
 
-        Debug.Log("_maxLineHeight= "+ _maxLineHeight+ "px, coressponds to _maxLivePeriod="+ _maxLivePeriod+" years");
+        Debug.Log("_maxLineHeight= "+ _maxLineHeight+ "px, coressponds to _maxLiveYears=" + _maxLiveYears+" years");
 
         InitPeopleQueue();
-        CalculateAlivePeriod();
+        CalculateMaxAliveYear();
     }
 
     /**
@@ -75,46 +75,44 @@ public class LineGenerator : MonoBehaviour
         for (int i = 0; i < _peopleAmount; i++)
         {
             Person person = new Person();
-            person.birthYear = (int)Random.Range((float)PERIOD_START, (float)(PERIOD_END));   // 1900 and 2000 - possible birth year
-            person.deathYear = (int)Random.Range((float)person.birthYear, (float)PERIOD_END); // birthYear and 2000 - possible death year, so person can be born and die at the same year
-            person.livePeriod = person.deathYear - person.birthYear;
-            person.liveLineHeight = person.livePeriod * _maxLineHeight / _maxLivePeriod;    //using proportion between pixels in the Image rectangular area and live period in years
-            Debug.Log("Person" + i + " birthYear= " + person.birthYear + ", deathYear= " + person.deathYear + ", livePeriod= " + person.livePeriod + " years, corresponds to liveLineHeight= "+ person.liveLineHeight + "px");
+            person.birthYear = (int)Random.Range((float)PERIOD_START_YEAR, (float)(PERIOD_END_YEAR));   // 1900 and 2000 - possible birth year
+            person.deathYear = (int)Random.Range((float)person.birthYear, (float)PERIOD_END_YEAR); // birthYear and 2000 - possible death year, so person can be born and die at the same year
+            person.liveYears = person.deathYear - person.birthYear;
+            person.liveLineHeight = person.liveYears * _maxLineHeight / _maxLiveYears;    //using proportion between pixels in the Image rectangular area and live period in years
+            Debug.Log("Person" + i + " birthYear= " + person.birthYear + ", deathYear= " + person.deathYear + ", liveYears= " + person.liveYears + " years, corresponds to liveLineHeight= "+ person.liveLineHeight + "px");
 
             _peopleQueue.Enqueue(person);
         }
     }
 
-    private void CalculateAlivePeriod()
+    private void CalculateMaxAliveYear()
     {
-      /*  int 
-        foreach (Person person in _peopleQueue)
-        {
-            if(person.birthYear >= _minYear)
-            {
-                
-            }
-        }*/
+    
     }
 
     /**
-     * Draws texture with a size 2x2 px in a rectangle with a size 1xliveLineHeight px 
+     * Draws texture with a size 2x2 px in a Rectangle with a size 1xliveLineHeight px 
      */
     public void OnGUI()
     {
-        //TODO: needs to be fixed starting posY
         float counter = 0;
         foreach (Person person in _peopleQueue)
         {
-            // Position of GUI layer is top left corner of the screen
-            float posX = counter + _bottomLeftPos.x; // starting X position for drawing the line (which is a rectangle with width=1px)
-            //
-            float yearsFromStartPointToDeath = person.deathYear - PERIOD_START; 
-            float heightFromStartPointToDeath = yearsFromStartPointToDeath * _maxLineHeight / _maxLivePeriod;
+            //NOTE: Position of GUI layer is top left corner of the screen so the direction of line drawing will go from top to bottom
 
-            float posY = Screen.height - _bottomLeftPos.y - heightFromStartPointToDeath;  
+            // upper X position of the line (which is a rectangle with width=1px)
+            float upperPosX = counter + _bottomLeftPos.x;
+
+            // convert Years from PERIOD_START_YEAR to Death year in to pixels
+            float yearsFromStartPointToDeath = person.deathYear - PERIOD_START_YEAR; 
+            float heightFromStartPointToDeath = yearsFromStartPointToDeath * _maxLineHeight / _maxLiveYears; // pixels which are corresponded to yearsFromStartPointToDeath
+
+            // upper Y position of the line (which is a rectangle with width=1px)
+            float upperPosY = Screen.height - _bottomLeftPos.y - heightFromStartPointToDeath;  
             GUI.color = graphColor;
-            GUI.DrawTexture(new Rect(new Vector2(posX, posY), new Vector2(1, person.liveLineHeight)), _texture);
+
+            //Draw a rectangle from Death year position to Birth year position
+            GUI.DrawTexture(new Rect(new Vector2(upperPosX, upperPosY), new Vector2(1, person.liveLineHeight)), _texture);
             counter++;
         }
 
